2265806a6e80a201258d616c32f6a6542e8f247d
arnold rodriguez <arnolddavidrd@gmail.com>
2026-08-23 18:41:28 -0500

    add show password functionality to login form

--- src/actions/products.ts
+++ src/actions/products.ts
@@ -2,16 +2,6 @@ import { defineAction } from "astro:actions";
 import { z } from "astro/zod";
 import { turso } from "../turso";
 
-/* <th>Nombre</th>
-<th>Marca</th>
-<th>IMEI</th>
-<th>Precio</th>
-<th>Stock</th>
-<th>Color</th>
-<th>Almacenamiento</th>
-<th>Estado</th> */
-
-
 export const products = {
   create: defineAction({
     accept: "form",
@@ -23,14 +13,13 @@ export const products = {
       stock: z.coerce.number().min(1, "El stock es requerido"),
       color: z.string().trim().min(1, "El color es requerido"),
       storage: z.string().trim().min(1, "El almacenamiento es requerido"),
-      status: z.string().trim().min(1, "El estado es requerido"),
+      status: z.string().trim().min(1, "El estado es requerido")
     }),
     handler: async (input, context) => {
       const { name, brand, imei, price, stock, color, storage, status } = input;
 
-
-      // INSERT INTO users (name, email) VALUES ('Alice', 'alice@example.com');
-      const { rows } = await turso.execute("INSERT INTO products (name, brand, imei, price, stock, color, storage, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+      const { rows } = await turso.execute(
+        "INSERT INTO products (name, brand, imei, price, stock, color, storage, status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
         [name, brand, imei, price, stock, color, storage, status]
       );
 
@@ -38,4 +27,4 @@ export const products = {
       return { success: true, message: "Producto creado exitosamente" };
     }
   })
-}
+};
